d63a3d999fec4b06abbea963e4e43623acf65eef
Shailendra K. Maurya <skdmaurya@yahoo.com>
2023-01-14 13:37:21 +0000

    added item placeholder on backend

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -145,7 +145,7 @@ router.post("/", auth.required, function(req, res, next) {
       }
 
       var item = new Item(req.body.item);
-
+      item.image = req.body.item.image || "/placeholder.png";
       item.seller = user;
 
       return item.save().then(function() {
